fde0a72dc5a2667edc12f2f7a905fbbc2ecb7402
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-08-05 19:22:43 -0300

    Capitalize adicionado

---
+++ src/utils/capitalize.ts
@@ -0,0 +1,7 @@
+export function capitalizeAll(text: string): string {
+  return text
+    .trim()
+    .split(/\s+/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
